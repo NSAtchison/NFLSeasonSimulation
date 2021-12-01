01821ee5e16441341016ec5164a2ceb081ada86f
natchison25 <natchison25@my.whitworth.edu>
2021-12-01 14:50:40 -0800

    Moved stuff out of main

---
+++ headers/team.h
@@ -0,0 +1,30 @@
+#ifndef TEAM_H
+#define TEAM_H
+#include <iostream>
+using namespace std;
+
+class Team {
+    public:
+    //Attributes
+        string team_name;
+        int team_num;
+        int team_div;
+        int team_conf;
+        int numWins;
+        int numGamesPlayed;
+        bool hasPlayed;
+        bool madePlayoffs;
+        int playoffSeed;
+
+    //Behaviors
+    //default constructor
+        Team();
+
+        //variable constructor
+        Team(string name, int digit, int divNum, int confNum);
+
+        //Print function
+        string print();
+};
+
+#endif
---
+++ source/team.cpp
@@ -0,0 +1,37 @@
+#include "..\headers\team.h" 
+#include <iostream>
+using namespace std;
+
+    
+Team::Team() {
+    team_name = "No Team";
+    team_num = 0;
+    team_div = 0;
+    team_conf = 0;
+    numWins = 0;
+    numGamesPlayed = 0;
+    hasPlayed = false;
+    madePlayoffs = false;
+    playoffSeed = 0;
+}
+
+//variable constructor
+Team::Team(string name, int digit, int divNum, int confNum) {
+    team_name = name;
+    team_num = digit;
+    team_div = divNum;
+    team_conf = confNum;
+    numWins = 0;
+    numGamesPlayed = 0;
+    hasPlayed = false;
+    madePlayoffs = false;
+    playoffSeed = 0;
+}
+
+//Print function
+string Team::print() {
+    string winString = to_string(numWins);
+    int numLosses = numGamesPlayed - numWins;
+    string lossString = to_string(numLosses);
+    return team_name + "(" + winString + "-" + lossString + ")";
+}
